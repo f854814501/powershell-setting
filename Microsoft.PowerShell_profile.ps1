b9533292e9a3f9bf7aed4488851fae9a08bfde56
f854814501 <854814501@qq.com>
2022-03-24 20:22:31 +0800

    my powershell setting

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,27 @@
+Import-Module oh-my-posh
+Import-Module ZLocation
+Import-Module PSReadLine
+Import-Module posh-git
+
+Set-PoshPrompt -Theme jtracey93
+
+#-------------------------------  Set Hot-keys BEGIN  -------------------------------
+
+# 导入Linux快捷键
+Set-PSReadLineOption -EditMode Emacs
+
+# 设置预测文本来源为历史记录
+Set-PSReadLineOption -PredictionSource History
+
+# 每次回溯输入历史，光标定位于输入内容末尾
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+
+# 设置 Tab 为菜单补全和 Intellisense
+Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
+
+# 设置向上键为后向搜索历史记录
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+
+# 设置向下键为前向搜索历史纪录
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+#-------------------------------  Set Hot-keys END    -------------------------------
